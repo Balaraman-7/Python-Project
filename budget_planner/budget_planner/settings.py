b00926e16e20b8584408184ad11d80024c9c63aa
Your Name <you@example.com>
2026-06-28 14:53:59 +0530

    adding files

--- budget_planner/budget_planner/settings.py
+++ budget_planner/budget_planner/settings.py
@@ -73,3 +73,11 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "expenses" / "static",
 ]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    ...
+]
